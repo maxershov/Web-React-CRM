63e2e33655c1e370988de7129f2c02b8b7b56f2e
MaxErshov <maxx95.95@gmail.com>
2020-01-20 20:29:24 +0300

    Add puppeteer test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,4 +1,30 @@
+import puppeteer from 'puppeteer';
+import "regenerator-runtime/runtime";
 import {getIndexByCode, getDaysLeft} from '../App';
+import myLocalHost from '../../host';
+
+let browser;
+let page;
+
+beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: false
+    });
+    page = await browser.newPage();
+    await page.goto(myLocalHost.pagePath);
+  });
+  
+  test("renders first link", async () => {
+    await page.waitForSelector("#root");
+  
+    const headerFirstLink = await page.$eval("#root > nav > a:nth-child(1)", e => e.innerHTML);
+    expect(headerFirstLink).toBe(`Главная`);
+  });
+  
+
+  afterAll(() => {
+    browser.close();
+  });
 
 test('get index by code', () => {
     const index = getIndexByCode('111111');
