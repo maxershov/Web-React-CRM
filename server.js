e53d739d4c2e4322cbcbf71a7513443e03926bb3
maxershov <maxx95.95@gmail.com>
2020-01-31 02:22:24 +0300

    Del 'compression' => routes doesn't work

--- server.js
+++ server.js
@@ -3,15 +3,12 @@
 const express = require("express");
 const history = require('connect-history-api-fallback');
 const path = require("path");
-const compression = require("compression");
 
 const port = process.env.PORT || 8080;
 const staticFiles = express.static(path.join(__dirname, "dist"));
 
 const app = express();
 app.use(staticFiles);
-app.use(compression);
 app.use(history());
 app.listen(port);
 app.use(staticFiles)
-console.log(`App is listening on port ${port}`);
