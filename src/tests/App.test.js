5ba0bd92d527c1e778faa92d514c9f6626e6cfd7
Max Ershov <maxx95.95@gmail.com>
2020-02-04 21:13:13 -0800

    Chg puppeteer to -core and upd packJson

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,37 +1,39 @@
-import puppeteer from 'puppeteer';
+// import puppeteer from './node-modules/puppeteer-core'
+import puppeteer from '../../node_modules/puppeteer-core';
 import "regenerator-runtime/runtime";
 import { getIndexByCode, getDaysLeft } from '../App';
 import myLocalHost from '../../host';
 
 
 // puppeteer test
-// let browser;
-// let page;
+let browser;
+let page;
 
-// beforeAll(async () => {
-//   browser = await puppeteer.launch({
-//     headless: false
-//   });
-//   page = await browser.newPage();
-//   await page.goto(myLocalHost.pagePath);
-// });
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\Chrome.exe',
+    headless: false,
+  });
+  page = await browser.newPage();
+  await page.goto(myLocalHost.pagePath);
+});
 
-// test("renders first link", async () => {
-//   await page.waitForSelector("#root");
+test("renders first link", async () => {
+  await page.waitForSelector("#root");
 
-//   const headerFirstLink = await page.$eval("#root > nav > a:nth-child(1)", e => e.innerHTML);
-//   expect(headerFirstLink).toBe(`Главная`);
-// });
+  const headerFirstLink = await page.$eval("#root > nav > a:nth-child(1)", e => e.innerHTML);
+  expect(headerFirstLink).toBe(`Главная`);
+});
 
-// afterAll(() => {
-//   browser.close();
-// });
+afterAll(() => {
+  browser.close();
+});
 
 
 // jest func tests
 
 test('get index by code', () => {
-  const index = getIndexByCode('111111');
+  const index = getIndexByCode('1');
   expect(index).toBe(0);
 })
 
