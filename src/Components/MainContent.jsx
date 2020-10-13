a824b738d63bdd1d59eca3ccde04e35b7587c22a
Max Ershov <maxx95.95@gmail.com>
2020-10-14 01:52:00 +0300

    fix: routes for subdirectory hosting

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,5 +1,5 @@
 import React from 'preact/compat';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import UserPage from './UserPage';
 import TablePage from './TablePage';
 import TablePageShort from './TablePageShort';
@@ -8,8 +8,7 @@ import MainPage from './MainPage';
 const MainContent = () => {
   return (
     <Switch>
-      <Redirect exact from="/" to="/main" />
-      <Route exact path="/main" component={MainPage} />
+      <Route exact path={["/crm/", "/"]} component={MainPage} />
       <Route exact path="/clients/page/:pageNum" component={TablePage} />
       <Route exact path="/lead/page/:pageNum" render={() => <TablePageShort tableType="ЛИД" />} />
       <Route exact path="/employee/page/:pageNum" render={() => <TablePageShort tableType="СОТРУДНИК" />} />
